b2442360c92f8678ba93931c0e818aee814f75d0
kishu_chopra <158042875+kishu1982@users.noreply.github.com>
2026-09-11 10:57:21 +0530

    ip cof cross origin added

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["typeorm", "mongodb", "ws"],
+  allowedDevOrigins: ["43.242.224.95"],
 };
 
 export default nextConfig;
